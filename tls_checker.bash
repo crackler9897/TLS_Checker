4062ee7a5b82fe82e2c0edad238878a390e2064e
crackler9897 <crackler@gmail.com>
2019-12-05 21:34:02 -0700

    finished baseline tls checker script

--- tls_checker.bash
+++ tls_checker.bash
@@ -3,39 +3,52 @@
 
 searchVip()
 {
-    x=$(tmsh -q -c 'cd /'"$env"'; show ltm virtual '"$1"' profiles' | grep 'TLS Protocol Version 1.0                                                            0')
-    y=$(tmsh -q -c 'cd /'"$env"'; show ltm virtual '"$1"' profiles' | grep 'TLS Protocol Version 1.1                                                            0')
-      if [ -n "$x" && -n "$y" ]; then
-          echo "$1" >> "$HOME"/TLS_1_2_only_"$env".txt
-        else 
+  x=$(tmsh -q -c 'cd /; show ltm virtual '"$1"' profiles' | grep -A 35 'Ltm::ClientSSL Profile' | grep 'TLS Protocol Version 1.0' | awk '{print $6}')
+  y=$(tmsh -q -c 'cd /; show ltm virtual '"$1"' profiles' | grep -A 35 'Ltm::ClientSSL Profile' | grep 'TLS Protocol Version 1.1' | awk '{print $6}')
+  z=$(tmsh -q -c 'cd /; show ltm virtual '"$1"' profiles' | grep 'Ltm::ClientSSL Profile')
+  
+  # echo "${x}, ${y}"
+
+  if [ -z "$z" ]; then
+        :
+    elif [ "$x" = 0 ] && [ "$y" = 0 ] && [ -n "$env" ]; then
+        echo "$1" >> "$HOME"/TLS_1_2_only_"$env".txt
+    elif [ "$x" = 0 ] && [ "$y" = 0 ] && [ -z "$env" ]; then
+        echo "$1" >> "$HOME"/TLS_1_2_only_all_partitions.txt
+    elif [ "$x" != 0 ] || [ "$y" != 0 ]; then
+        if [ -n "$env" ]; then
           echo "$1" >> "$HOME"/TLS_offenders_"$env".txt
-      fi
+        fi
+    else
+        echo "$1" >> "$HOME"/TLS_offenders_all_partitions.txt
+  fi
 }
 
 pauseIt()
 {
-  sleep .5
-  echo -n "*"
-  sleep .5
-  echo -n "*"
-  sleep .5
-  echo -n "*"
-  sleep .5
   echo ""
   echo ""
+  echo ""
+  sleep .5
+  echo -n "*"
+  sleep .5
+  echo -n "*"
+  sleep .5
+  echo -n "*"
+  sleep .5
 }
 
 partitions=($(tmsh -q -c 'cd /; list auth partition one-line' | awk '{ print $3 }'))
+
+pauseIt
 
 echo ""
 echo ""
 echo ""
 echo "Partitions: ${partitions[@]} *ALL*"
 echo ""
-  
-pauseIt() 
 
-read -p "What partition should we check?...<enter> to check all partitions: " env
+read -p "What partition should we check?...<enter> key to check all partitions: " env
 sleep .5
 echo ""
 
@@ -45,7 +58,10 @@ if [ -n "$env" ]; then
       echo "env defined is /"
 fi
 
-pauseIt()
+pauseIt
+echo ""
+echo ""
+echo ""
 
 vipsArr=($(tmsh -q -c 'cd /'"$env"'; list ltm virtual one-line recursive' | awk '{print $3}' ))
 echo "${vipsArr[@]}"
@@ -55,7 +71,7 @@ for i in ${vipsArr[@]}; do
     i="${env}/${i}"
   fi
   echo "$i"
-  #searchVip($i)
+  searchVip "$i"
 done
 
 
